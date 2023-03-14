ebbbd2feeb30f6b9add7ccb4a41c59297c2189a5
Matheus Santos <111880593+math3usSan@users.noreply.github.com>
2023-03-14 09:18:54 -0300

    estruturação do projeto

---
+++ costs/src/App.js
@@ -0,0 +1,32 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </ul>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </BrowserRouter>
+  );
+}
+
+export default App;
