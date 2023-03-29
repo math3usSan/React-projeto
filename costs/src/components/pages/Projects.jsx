42c06c233917fee6661cec7586c6f7f19e0777f9
Matheus Santos <111880593+math3usSan@users.noreply.github.com>
2023-03-29 10:21:04 -0300

    loader

--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 
 import Message from "./Message"
 import Container from '../layout/Container'
+import Loading from '../layout/Loading'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
 
@@ -12,6 +13,7 @@ import styles from './Projects.module.css'
 function Projects() {
 
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -20,8 +22,8 @@ function Projects() {
     }
 
     useEffect(() => {
-
-        fetch('http://localhost:5000/projects', {
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -30,8 +32,10 @@ function Projects() {
         .then(data => {
             console.log(data)
             setProjects(data)
+            setRemoveLoading(true)
         })
         .catch((err) => console.log(err))
+        }, 300)
     }, [])
 
     return (
@@ -52,6 +56,10 @@ function Projects() {
                         key={project.id}
                         />
                     ))}
+                    {!removeLoading && <Loading/> }
+                    {removeLoading && projects.length === 0 && (
+                        <p>Não há projetos cadastrados!</p>
+                    )}
             </Container>
         </div>
     )
